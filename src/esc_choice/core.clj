f791bee3ec28d42c883448782575ba60a444b2cf
Stefan du Fresne <stefan@sdufresne.info>
2015-11-21 16:19:05 +0000

    Add TZ support

--- src/esc_choice/core.clj
+++ src/esc_choice/core.clj
@@ -1,17 +1,24 @@
 (ns esc-choice.core
   (:require [clj-time.core :as t])
-  (:import [org.joda.time LocalDateTime LocalTime]))
+  (:import [org.joda.time DateTime DateTimeZone LocalTime]))
+
+(defn localtime-contained?
+  "Is the instant contained in the defined range?"
+  [^LocalTime instant-time [^LocalTime start-time ^LocalTime end-time]]
+  (or (= instant-time start-time)
+      (= instant-time end-time)
+      (if (t/before? start-time end-time)
+          (and (t/before? start-time instant-time) (t/after? end-time instant-time))
+          (or (and (t/before? end-time instant-time) (t/before? start-time instant-time))
+              (and (t/after? end-time instant-time) (t/after? start-time instant-time))))))
 
 (defn time-contained?
-  "Is the instant contained in the defined range?"
-  [^LocalDateTime instant [^LocalTime start-time ^LocalTime end-time]]
-  (let [instant-time (.toLocalTime instant)]
-    (or (= instant-time start-time)
-        (= instant-time end-time)
-        (if (t/before? start-time end-time)
-            (and (t/before? start-time instant-time) (t/after? end-time instant-time))
-            (or (and (t/before? end-time instant-time) (t/before? start-time instant-time))
-                (and (t/after? end-time instant-time) (t/after? start-time instant-time)))))))
+  [^DateTime instant time-range ^DateTimeZone user-tz]
+  (localtime-contained? (-> instant
+                            (.withZone user-tz)
+                            .toLocalTime)
+                        time-range))
+
 
 ; (defn choice-algo
 ;   [people current-time]
--- test/esc_choice/core_test.clj
+++ test/esc_choice/core_test.clj
@@ -1,21 +1,33 @@
 (ns esc-choice.core-test
   (:require [clojure.test :refer :all]
             [esc-choice.core :refer :all]
-            [clj-time.core :as t]))
+            [clj-time.core :as t])
+  (:import [org.joda.time DateTimeZone]))
 
-(defn dt [& params] (apply t/local-date-time (concat [2015 11 11] params)))
-(defn t [& params] (apply t/local-time params))
+(defn tz [tz-string] (t/time-zone-for-id tz-string))
+(defn dt [tz-string & params] (t/from-time-zone
+  (apply t/date-time (concat [2015 11 11] params))
+  (tz tz-string)))
+(defn ldt [& params] (apply t/local-date-time (concat [2015 11 11] params)))
+(defn lt [& params] (apply t/local-time params))
 
-(deftest time-contained-test
-  (testing "time-contained? works generally"
-    (is      (time-contained? (dt 11) [(t 10) (t 12 30)]))
-    (is (not (time-contained? (dt 11) [(t 12) (t 14)]))))
+(deftest localtime-contained-test
+  (testing "localtime-contained? works generally"
+    (is      (localtime-contained? (lt 11) [(lt 10) (lt 12 30)]))
+    (is (not (localtime-contained? (lt 11) [(lt 12) (lt 14)]))))
   (testing "including across the midnight boundary"
-    (is      (time-contained? (dt 11) [(t 10) (t 3)]))
-    (is (not (time-contained? (dt 11) [(t 23) (t 3)])))
-    (is      (time-contained? (dt 11) [(t 23) (t 12)])))
-  (testing "time-contained? contains boundaries"
-    (is      (time-contained? (dt 11 12 13) [(t 11 12 13) (t 12)]))
-    (is      (time-contained? (dt 11 12 13) [(t 10) (t 11 12 13)]))))
+    (is      (localtime-contained? (lt 11) [(lt 10) (lt 3)]))
+    (is (not (localtime-contained? (lt 11) [(lt 23) (lt 3)])))
+    (is      (localtime-contained? (lt 11) [(lt 23) (lt 12)])))
+  (testing "localtime-contained? contains boundaries"
+    (is      (localtime-contained? (lt 11 12 13) [(lt 11 12 13) (lt 12)]))
+    (is      (localtime-contained? (lt 11 12 13) [(lt 10) (lt 11 12 13)]))))
 
-;; TODO work out how to do a core.check test on time-contained? to catch edge-cases
+(deftest datetime-contained?
+  (testing "datetime-contained? works generally"
+    (is      (time-contained? (dt "UTC" 10 30) [(lt 10) (lt 12)] (tz "UTC"))))
+  (testing "datetime-contained? understands timezones!"
+    (is (not (time-contained? (dt "UTC" 10 30) [(lt 10) (lt 12)] (tz "NZ"))))
+    (is      (time-contained? (dt "UTC" 10 30) [(lt 22) (lt 0)] (tz "NZ")))))
+
+;; TODO work out how to do a core.check test on localtime-contained? to catch edge-cases
